eedcc5816da923a36e2cc326e9db4020f4496153
BudiSalah <budi.salah@outlook.com>
2020-06-25 11:09:43 +0200

    Finish the responsive and fix som others bugs #

--- part 1/js/script.js
+++ part 1/js/script.js
@@ -3,7 +3,7 @@ function showHideLayer() {
     let layerParent = document.getElementById("enough-section-wrapper");
     if (!layerParent) {return;}
     window.addEventListener("scroll", e => {
-        let checkOffset = (layerParent.offsetTop - 240) <= window.pageYOffset;       
+        let checkOffset = (layerParent.offsetTop - 240) <= window.pageYOffset && window.outerWidth > 991;       
         checkOffset ? layerParent.classList.add("remove") : layerParent.classList.remove("remove");
     })
 }
